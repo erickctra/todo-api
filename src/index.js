ed1e4122099b250d07e4aa6b4ec66ce25879d578
Erick Cintra <developer.erickcintra@outlook.com>
2022-04-04 12:35:01 -0300

    create user done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,42 @@
 const express = require("express");
-const { uuidv4 } = require("uuid");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 app.use(express.json());
+
+const users = [];
+
+function checksExistsUserAccount(request, response, next) {
+  const { username } = request.headers;
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return response.status(400).json({ error: "User not found!" });
+  }
+
+  request.user = user;
+  return next();
+}
+
+app.post("/user", (request, response) => {
+  const { name, username } = request.body;
+
+  const usernameAlreadyExists = users.some(
+    (user) => user.username === username
+  );
+
+  if (usernameAlreadyExists) {
+    return response.status(400).json({ error: "Username already exists" });
+  }
+
+  users.push({
+    name,
+    username,
+    id: uuidv4,
+    pro: false,
+  });
+
+  return response.status(201).send();
+});
+
+app.listen(3333);
