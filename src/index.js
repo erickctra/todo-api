b1611ca058f81708ec3cb3c32d2304d9af987c54
Erick Cintra <developer.erickcintra@outlook.com>
2022-04-04 12:55:42 -0300

    create todo done

--- src/index.js
+++ src/index.js
@@ -11,10 +11,22 @@ function checksExistsUserAccount(request, response, next) {
   const user = users.find((user) => user.username === username);
 
   if (!user) {
-    return response.status(400).json({ error: "User not found!" });
+    return response.status(404).json({ error: "User not found!" });
   }
 
   request.user = user;
+  return next();
+}
+
+function checksCreateTodosUserAvailability(request, respone, next) {
+  const { user } = request;
+
+  if (user.todo.length === 9 && user.pro === false) {
+    return response
+      .status(406)
+      .json({ unauthorized: "Free plan has limit to 10 todos" });
+  }
+
   return next();
 }
 
@@ -32,11 +44,31 @@ app.post("/user", (request, response) => {
   users.push({
     name,
     username,
-    id: uuidv4,
+    id: uuidv4(),
     pro: false,
+    todo: [],
   });
 
   return response.status(201).send();
 });
+
+app.post(
+  "/todo",
+  checksExistsUserAccount,
+  checksCreateTodosUserAvailability,
+  (request, response) => {
+    const { title, description } = request.body;
+    const { user } = request;
+
+    user.todo.push({
+      title,
+      description,
+      done: false,
+      id: uuidv4(),
+    });
+
+    return response.status(201).send();
+  }
+);
 
 app.listen(3333);
